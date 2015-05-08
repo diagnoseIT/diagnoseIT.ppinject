e958ce6545c21157a402fc8892613f12b3c043f3
Thomas Düllmann <duellmtk@studi.informatik.uni-stuttgart.de>
2015-05-08 18:34:08 +0200

    Removed unused imports from Container.java

--- ppinject/src/main/java/com/github/diagnoseit/ppinject/Container.java
+++ ppinject/src/main/java/com/github/diagnoseit/ppinject/Container.java
@@ -1,15 +1,7 @@
 package com.github.diagnoseit.ppinject;
 
-import java.lang.management.ManagementFactory;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-
-import javax.management.InstanceAlreadyExistsException;
-import javax.management.MBeanRegistrationException;
-import javax.management.MBeanServer;
-import javax.management.MalformedObjectNameException;
-import javax.management.NotCompliantMBeanException;
-import javax.management.ObjectName;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
